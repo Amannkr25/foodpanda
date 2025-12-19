96a098202557ffc5007db531a4edea5dc5c0ba8a
Amannkr25 <amannkr0506@gmail.com>
2025-12-19 23:51:44 +0530

    Correct the Add/update action button

--- src/component/SubitemCards.tsx
+++ src/component/SubitemCards.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { food } from './ItemCards';
 import { log } from 'node:console';
 
-type setState=()=>void;
+
 export default function SubitemCards({
   picked,
   count,
@@ -13,9 +13,9 @@ export default function SubitemCards({
   setEdit, editItem,
   setPicked
 }
-  : { picked: food | null, count: number, setItemlist: any, setCount: any, itemlist: any, setEdit: any, editItem: any,setPicked:any }) {
+  : { picked: food | null, count: number, setItemlist: any, setCount: any, itemlist: any, setEdit: any, editItem: any, setPicked: any }) {
 
-    const Sider = [
+  const Sider = [
     {
       info: "Extra Cheese",
       price: 20
@@ -27,9 +27,9 @@ export default function SubitemCards({
       price: 25
 
     }]
-//HOOKS :
+  //HOOKS :
   const [checked, setChecked] = useState<string[]>([]);
-    const [snack, setSnack] = useState<{
+  const [snack, setSnack] = useState<{
     open: boolean
     message: string
     severity: 'success' | 'info' | 'warning' | 'error'
@@ -39,7 +39,7 @@ export default function SubitemCards({
     severity: 'info',
   })
 
-//FUNCTIONALITY:
+  //FUNCTIONALITY:
   const handleToggle = (value: string) => {
     setChecked((prev: string[]) =>
       prev.includes(value)
@@ -48,7 +48,7 @@ export default function SubitemCards({
     );
   };
 
-    const openSnack = (
+  const openSnack = (
     message: string,
     severity: 'success' | 'info' | 'warning' | 'error' = 'info'
   ) => {
@@ -64,81 +64,88 @@ export default function SubitemCards({
     if (editItem) {
       setCount(editItem.count)
       setChecked(editItem.extra?.map((e: any) => e.info) || [])
-      setPicked({id:editItem.id,name:editItem.name,price:editItem.price})
+      setPicked({ id: editItem.id, name: editItem.name, price: editItem.price })
 
-        openSnack('Editing item', 'info')
-      
+      openSnack('Editing item', 'info')
+
     }
   }, [editItem])
 
   const addItems = () => {
 
     if (!picked) {
-      openSnack('Add item First','warning')
-      return};
+      openSnack('Add item First', 'warning')
+      return
+    };
 
-    const existedItem = itemlist?.find((list: any) => list.id === picked.id)
-    console.log(existedItem);
+
+
 
     const SelecteSider = Sider.filter(s => checked.includes(s.info))
 
-     if (editItem) {
+    if (editItem) {
       setItemlist((prev: food[]) =>
         prev.map(item =>
-          item.id === picked.id
-            ? { ...item,name:picked.id, count, extra: SelecteSider }
+          item.id === editItem.id
+            ? { ...item, count, extra: SelecteSider }
             : item
         )
       )
-       setEdit(null)
+      setEdit(null)
       setChecked([])
       setCount(1)
 
-           openSnack('Item updated successfully', 'success')
+      openSnack('Item updated successfully', 'success')
 
       return
     }
+    else {
 
+      const existedItem = itemlist?.find((list: any) => list.id === picked.id)
 
-    if (existedItem) {
-      openSnack('Item already added!', 'warning')
-      return;
+      if (existedItem) {
+        openSnack('Item already added!', 'warning')
+        return;
+      }
+
     }
 
-    
+
 
     const FullItemList = {
       ...picked, count, extra: SelecteSider
     }
-           
+
     setItemlist((prev: food[] | null) => [...(prev || []), FullItemList])
 
-      setChecked([])
+    setChecked([])
     setCount(1)
-     openSnack('Item added successfully', 'success')
+    openSnack('Item added successfully', 'success')
 
   }
   const clearRecord = () => {
     setItemlist(null)
     setChecked([])
     setCount(0)
-     openSnack('Selection cleared', 'info')
+    openSnack('Selection cleared', 'info')
 
 
   }
-  
+
   return (
     <Box sx={{ height: "27%", width: "100%", display: "grid", gridTemplateColumns: "2fr 1fr", gap: .5 }}>
 
-      <Box sx={{ p:1,height: "100%",width:"100%" ,bgcolor:"transparent", borderRadius: 3 }}>
+      <Box sx={{ p: 1, height: "100%", width: "100%", bgcolor: "transparent", borderRadius: 3 }}>
 
-        <List sx={{ width:"100%" }}>
+        <List sx={{ width: "100%" }}>
           {
             Sider.map((value) => (
               <ListItem key={value.info} disablePadding >
-                <ListItemButton sx={{ ":hover": { bgcolor: "#826018ff"  },
-                 bgcolor: `${checked.includes(value.info) ? "#826018ff"  : "#b37838ff"}`,
-                  mb: .2, borderRadius: 3 }} onClick={() => handleToggle(value.info)} dense>
+                <ListItemButton sx={{
+                  ":hover": { bgcolor: "#826018ff" },
+                  bgcolor: `${checked.includes(value.info) ? "#826018ff" : "#b37838ff"}`,
+                  mb: .2, borderRadius: 3
+                }} onClick={() => handleToggle(value.info)} dense>
                   <ListItemIcon>
                     <Checkbox checked={checked.includes(value.info)} />
                   </ListItemIcon>
@@ -157,18 +164,18 @@ export default function SubitemCards({
 
 
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", m: 1, bgcolor:"transparent", borderRadius: 3, gap: 1 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", m: 1, bgcolor: "transparent", borderRadius: 3, gap: 1 }}>
 
         <Button variant="contained" color="success"
           onClick={(e) => addItems()}
           sx={{ ml: 3, px: 3, py: 2.5, borderRadius: 4, fontSize: "20px" }}
-        >   Add</Button>
+        >   {editItem ? "Update" : "Add"}</Button>
         <Button variant="contained" color="error"
           sx={{ ml: 3, px: 3, py: 1.5, borderRadius: 4, fontSize: "20px" }}
           onClick={() => clearRecord()}
         >  Cancel</Button>
       </Box>
-           <Snackbar
+      <Snackbar
         open={snack.open}
         autoHideDuration={2500}
         onClose={closeSnack}
